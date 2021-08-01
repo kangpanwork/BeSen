c4eb7d4413b1e82b349f7c2d381f8d7123c8b140
kangpanwork <kangpan_work@163.com>
2021-08-02 07:15:18 +0800

    update

--- src/main/java/beSen/future/simpleDesign/Client.java
+++ src/main/java/beSen/future/simpleDesign/Client.java
@@ -4,4 +4,15 @@ package beSen.future.simpleDesign;
  * @author 康盼Java开发工程师
  */
 public class Client {
+    public Data request(final String queryStr) {
+        final FutureData future = new FutureData();
+        new Thread() {
+            @Override
+            public void run() {
+                RealData realData = new RealData(queryStr);
+                future.setRealData(realData);
+            }
+        }.start();
+        return future;
+    }
 }
--- src/main/java/beSen/future/simpleDesign/FutureMain.java
+++ src/main/java/beSen/future/simpleDesign/FutureMain.java
@@ -5,6 +5,12 @@ package beSen.future.simpleDesign;
  */
 public class FutureMain {
     public static void main(String[] args) {
-
+        Client client = new Client();
+        Data data = client.request("name");
+        System.out.println("请求完毕");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e){}
+        System.out.println("数据=" + data.getResult());
     }
 }
