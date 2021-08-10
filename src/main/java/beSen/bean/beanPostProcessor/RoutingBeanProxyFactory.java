a6e80f748a8fca50ac8b264c71c11f61ef229903
kangpanwork <kangpan_work@163.com>
2021-08-10 23:01:10 +0800

    update

---
+++ src/main/java/beSen/bean/beanPostProcessor/RoutingBeanProxyFactory.java
@@ -0,0 +1,10 @@
+package beSen.bean.beanPostProcessor;
+
+/**
+ * @author 康盼Java开发工程师
+ */
+public class RoutingBeanProxyFactory {
+    public static Object createProxy(Class targetClass, Class beanClass) {
+        return null;
+    }
+}
---
+++ src/main/java/beSen/bean/beanPostProcessor/Version.java
@@ -0,0 +1,20 @@
+package beSen.bean.beanPostProcessor;
+
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+/**
+ * @author 康盼Java开发工程师
+ */
+@Target({ElementType.FIELD,ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Component
+public @interface Version {
+}
